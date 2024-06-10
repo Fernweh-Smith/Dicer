bb0a2b9d4e95ee01da743c652fcc557d5f5f7369
Fernweh-Smith <104039346+Fernweh-Smith@users.noreply.github.com>
2024-06-10 21:44:07 +1000

    Refactored dice input validation tests. Testing is now more modular making it easier to add other validation tests

--- src/tests.cpp
+++ src/tests.cpp
@@ -2,96 +2,112 @@
 #include "cli_args.h"
 
 #include <iostream>
+#include <functional>
 
-struct DiceNotationTestData
+using InputTestData = std::vector<std::string>;
+using InputValidationFuncSignature = std::function<bool(const std::string_view&)>;
+
+InputTestData create_valid_dice_input_data()
 {
-    std::string input;
-    bool is_valid;
-};
+    return std::vector<std::string> 
+    {
+        "1d20",
+        "12d1",
+        "d10",
+        "d10+12",
+        "1d20+12",
+        "1d20-21",
+    };
+}
 
-// using DiceNotationTestResult = std::pair<DiceNotationTestData, bool>;
+InputTestData create_invalid_dice_input_data()
+{
+    return std::vector<std::string>
+    {
+    "3c23",
+    "14dx12",
+    "1d20+",
+    "1d20-",
+    "1d20+12x",
+    "1d",
+    "d20+1d12",
+    "d20+",
+    "d+1",
+    "1d+1",
+    "4d+11",
+    "4x+11",
+    "x1d20",
+    "1xd20",
+    "1d2v0",
+    "4d+1.1",
+    };
+}
+
+size_t get_longest_input(const InputTestData& test_data)
+{
+    const auto start_iter = test_data.cbegin();
+    const auto end_iter = test_data.cend();
+    const auto compare_func = [](const std::string& a, const std::string& b){ return a.length() < b.length(); };
+    const auto element_iter =  std::max_element(start_iter, end_iter, compare_func);
+    return element_iter->length();
+}
+
+bool validate_input_data(const InputTestData& inputs, 
+    InputValidationFuncSignature tester_function,
+    const bool inputs_valid)
+{
+    size_t max_input_length = get_longest_input(inputs);
+
+    const size_t valid_msg_pad = 2;
+    const std::string msg_valid   = "Valid";
+    const std::string msg_invalid = "Invalid";
+    const std::string msg_passed  = "Passed";
+    const std::string msg_failed  = "Failed";
+    const std::string msg_separator  = " | ";
+
+    bool all_tests_match = true;
+
+    for (const auto &input : inputs)
+    {
+        bool test_says_valid = tester_function(input);
+        bool results_match = test_says_valid == inputs_valid;
+
+        if(!results_match) { all_tests_match = false; }
+
+        results_match ? (std::cout << msg_passed) : (std::cout << msg_failed);
+        std::cout << msg_separator;
+        std::cout << input << std::string(max_input_length - input.length(), ' ');
+        std::cout << msg_separator;
+        std::cout << "Expected Result: ";
+        inputs_valid ? (std::cout << msg_valid << std::string(valid_msg_pad, ' ')) : (std::cout << msg_invalid);
+        std::cout << msg_separator;
+        std::cout << "Test Result: ";
+        test_says_valid ? (std::cout << msg_valid << std::string(valid_msg_pad, ' ')) : (std::cout << msg_invalid);
+        std::cout << '\n';
+    }
+
+    return all_tests_match;
+}
 
 void test_dice_notation_recognition()
 {
-    const std::vector<DiceNotationTestData> test_data
-    {
-        {"1d20", true},
-        {"12d1", true},
-        {"d10", true},
-        {"d10+12", true},
-        {"1d20+12", true},
-        {"1d20-21", true},
-        
-        {"3c23", false},
-        {"14dx12", false},
-        {"1d20+", false},
-        {"1d20-", false},
-        {"1d20+12x", false},
-        {"1d", false},
-        {"d20+1d12", false},
-        {"d20+", false},
-        {"d+1", false},
-        {"1d+1", false},
-        {"4d+11", false},
-        {"4x+11", false},
-        {"x1d20", false},
-        {"1xd20", false},
-        {"1d2v0", false},
-        {"4d+1.1", false},
-    };
-
-    int longest_input = std::max_element(test_data.cbegin(), test_data.cend(), 
-    [](const DiceNotationTestData& a, const DiceNotationTestData& b)
-        { return a.input.length() < b.input.length(); })->input.length();
-
-    const std::string valid = "Valid";
-    const std::string invalid = "Invalid";
-
-    std::cout << "Dice Notation Recognition Tests.\n";
-
-    bool all_tests_passed = true;
-
-    for (const auto &data : test_data)
-    {
-        const bool test_result = CLI::is_dice_notation(data.input);
-        const bool expected_result = data.is_valid;
-        const bool results_match = test_result == expected_result;
-
-        std::cout << "Results Match: ";
-
-        if(results_match)
-        {
-            std::cout << "Yes";
-        }
-        else
-        {
-            std::cout << "No ";
-            all_tests_passed = false;
-        }
-
-        std::cout << " | Input: " << data.input << std::string(longest_input+1 - data.input.length(), ' ') << "| Expected Result: ";
-        if(data.is_valid)
-        {
-            std::cout << valid << "  ";
-        }
-        else
-        {
-            std::cout << invalid;
-        }
-
-        std::cout << " | Test Result: ";
-
-        if(CLI::is_dice_notation(data.input))
-        {
-            std::cout << valid << "  ";
-        }
-        else
-        {
-            std::cout << invalid;
-        }
-        std::cout << '\n';
-    }
-    std::cout << "All Tests Passed: " << (all_tests_passed ? "Yes" : "No") << std::endl;
+    const InputValidationFuncSignature& test_func = CLI::is_dice_notation;
+    std::cout << "Dice Notation Validation Test\n";
+    std::cout << "Valid Inputs:\n";
+    bool all_valid_inputs_passed = validate_input_data(
+        create_valid_dice_input_data(),
+        test_func,
+        true
+    );
+    std::cout << "Invalid Inputs:\n";
+    bool all_invalid_inputs_passed = validate_input_data(
+        create_invalid_dice_input_data(),
+        test_func,
+        false
+    );
+    const bool all_tests_passed = all_valid_inputs_passed && all_invalid_inputs_passed;
+    all_tests_passed ? (std::cout << "All tests passed!") : (std::cout << "Some tests failed.");
+    std::cout << std::endl;
 }
 
 void Tests::test_input_recognition()
